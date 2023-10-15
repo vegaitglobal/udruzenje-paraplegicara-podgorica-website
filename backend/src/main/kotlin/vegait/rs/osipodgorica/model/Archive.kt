666567e7317ac5ce5ff633838718b75da749d09e
djuroradusinovic <djuro.radusinovic@vegait.rs>
2023-10-15 14:25:24 +0200

    added Archive.kt table

---
+++ backend/src/main/kotlin/vegait/rs/osipodgorica/model/Archive.kt
@@ -0,0 +1,16 @@
+package vegait.rs.osipodgorica.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "archive")
+data class Archive(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    val description: String,
+    @Column(unique = true)
+    val path: String,
+    @Column(unique = true)
+    val name: String,
+)
