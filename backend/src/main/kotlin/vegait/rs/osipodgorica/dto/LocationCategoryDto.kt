37a3acd511747a0609b2387b4b90d8f5e9f3f3c7
d-vucetic <d.vucetic@vegait.rs>
2023-11-12 19:26:57 +0100

    location category dto

---
+++ backend/src/main/kotlin/vegait/rs/osipodgorica/dto/LocationCategoryDto.kt
@@ -0,0 +1,8 @@
+package vegait.rs.osipodgorica.dto
+
+class LocationCategoryDto(
+    var locationId: Long?,
+    var locationName: String,
+    var categoryName: String,
+    val categoryId: Long?
+)
